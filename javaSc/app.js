3e24681335c7270c45e5bc40bedf54f543a2fac2
unknown <jonykadhem@gmail.com>
2026-06-29 23:00:19 +0300

    option buttons are working

--- javaSc/app.js
+++ javaSc/app.js
@@ -14,10 +14,11 @@ const noQuest = document.querySelector('#noOfQuestions')
 const heart = document.querySelector('#hearts')
 const quesiton = document.querySelector('#quesiton')
 const answerDiv = document.querySelector('#answers')
+const startH1 = document.createElement('h1')
 const startBtn = document.querySelector('#start')
 const answerp = document.createElement('p')
-const startH1 = document.createElement('h1')
 const alphaBody = document.querySelector('#alphaBody')
+const winOrlosep = document.createElement('p')
 /*----------------------------- Event Listeners -----------------------------*/
 startH1.textContent = 'Are You Ready !!!!!'
 alphaBody.append(startH1)
@@ -28,8 +29,8 @@ startBtn.addEventListener('click',function (event) {
     // answerp.textContent = 
     answerOptions(alphaQuestArr[randomQuest].options)
     // answerDiv.append(answerp)
-    event.target.remove(startBtn)
-    
+    event.target.remove()
+    startH1.remove()
     
 })
 /*-------------------------------- Functions --------------------------------*/
@@ -39,17 +40,25 @@ startBtn.classList = 'start'
 // function for the options button
 function answerOptions(options) {
     // console.log(options)
+
     options.forEach((element) => {
         const optionBtn = document.createElement('button')
         optionBtn.innerText = element
         answerDiv.appendChild(optionBtn)
-        
-        optionBtn.addEventListener('click', function (correctOption) {
-            if (correctOption.innerText === alphaQuestArr.answer) {
-                console.log('correct')
+        optionBtn.classList = 'options-btn'       
+        optionBtn.addEventListener('click', function () {
+            if (optionBtn.innerText === alphaQuestArr[randomQuest].answer) {
+                winOrlosep.textContent= 'correct'
+                answerDiv.append(winOrlosep)
             }else{
-                console.log('incorrect')
+                winOrlosep.textContent= `incorrect😢. the correct answer is ${alphaQuestArr[randomQuest].answer}`
+                answerDiv.append(winOrlosep)
+                
             }
+            const button = document.querySelectorAll('.options-btn')
+            button.forEach(button => {
+                button.disabled = true
+            })
         })
         
     });
