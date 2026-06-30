87b47ad0dedb3534d9324e06dbf0268cfd3ba82e
unknown <jonykadhem@gmail.com>
2026-06-30 10:47:10 +0300

    the scores are now working

--- javaSc/app.js
+++ javaSc/app.js
@@ -1,5 +1,6 @@
 /*-------------------------------- Constants --------------------------------*/
 let randomQuest = 0
+
 /*-------------------------------- Variables --------------------------------*/
 const alphaQuestArr = [{quist:'What is the first letter in the Alphabets', answer:'A', options:['A', 'B', 'C']},
     {quist:'What word starts with the letter "A"',answer:'Apple', options:['Melon', 'Apple','Barry']}
@@ -7,6 +8,9 @@ const alphaQuestArr = [{quist:'What is the first letter in the Alphabets', answe
 const numQuestArr = []
 const colorQuestArr = []
 const animleQuestArr = []
+let countScore = 0
+let countHeart = 0
+let previousQuist = -1
 
 /*------------------------ Cached Element References ------------------------*/
 const score = document.querySelector('#score')
@@ -19,13 +23,26 @@ const startBtn = document.querySelector('#start')
 const answerp = document.createElement('p')
 const alphaBody = document.querySelector('#alphaBody')
 const winOrlosep = document.createElement('p')
+const nextQuist = document.createElement('button')
+const tryagain = document.createElement('button')
+
 /*----------------------------- Event Listeners -----------------------------*/
+nextQuist.textContent = 'Next'
+nextQuist.style.display = 'none'
+alphaBody.append(nextQuist)
+
 startH1.textContent = 'Are You Ready !!!!!'
 alphaBody.append(startH1)
 startBtn.addEventListener('click',function (event) {
-    randomQuest = Math.floor(Math.random()*2)
+    do{
+        randomQuest = Math.floor(Math.random()*2)
+
+    }while (randomQuest === previousQuist) 
+    previousQuist = randomQuest
     console.log(randomQuest)
     quesiton.textContent = alphaQuestArr[randomQuest].quist
+    countHeart = 3
+    countScore = 0
     // answerp.textContent = 
     answerOptions(alphaQuestArr[randomQuest].options)
     // answerDiv.append(answerp)
@@ -33,9 +50,13 @@ startBtn.addEventListener('click',function (event) {
     startH1.remove()
     
 })
-/*-------------------------------- Functions --------------------------------*/
 startBtn.classList = 'start'
 
+/*-------------------------------- Functions --------------------------------*/
+// function randomIndx() {
+    
+// }
+// randomQuest = randomIndx()
 
 // function for the options button
 function answerOptions(options) {
@@ -48,20 +69,33 @@ function answerOptions(options) {
         optionBtn.classList = 'options-btn'       
         optionBtn.addEventListener('click', function () {
             if (optionBtn.innerText === alphaQuestArr[randomQuest].answer) {
-                winOrlosep.textContent= 'correct'
+                winOrlosep.textContent= 'correct🎉, keep going 💪😍'
                 answerDiv.append(winOrlosep)
+                countScore += 1
             }else{
                 winOrlosep.textContent= `incorrect😢. the correct answer is ${alphaQuestArr[randomQuest].answer}`
                 answerDiv.append(winOrlosep)
-                
+                countHeart --
             }
             const button = document.querySelectorAll('.options-btn')
             button.forEach(button => {
                 button.disabled = true
             })
+        nextQuist.style.display = 'block'
+
         })
         
     });
 
+    score.textContent = `⭐ ${countScore}`
+    heart.textContent = `❤ ${countHeart}`
+    // console.log(countHeart,countScore)
     
+}
+
+function nextQuestion() {
+    nextQuist.addEventListener('click', function (next) {
+        quesiton.textContent = ''
+        optionBtn=''
+    })
 }
